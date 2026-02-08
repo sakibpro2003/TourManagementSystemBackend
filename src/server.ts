0e1446b1c1e0b75056568f353b13a8e0ebe237ff
sakibpro2003 <sakibprodhan2003@gmail.com>
2026-02-08 22:43:48 +0600

    fix: credentials hide

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://sakib:sakib2@cluster0.cphe2d0.mongodb.net/HealthSyncHospital?retryWrites=true&w=majority&appName=Cluster0",
-    );
+    await mongoose.connect(envVars.DB_URL!);
     console.log("DB connected !");
-    server = app.listen(5000, () => {
+    server = app.listen(envVars.PORT, () => {
       console.log("Server is running on port 5000");
     });
   } catch (error) {
